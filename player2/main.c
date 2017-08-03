f97213dfcd5a89dae825ede85d7bdd4550bd2924
zy <zy>
2017-08-03 18:55:33 +0800

    Initial Commit

---
+++ player2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  player2
+//
+//  Created by zhangyun on 03/08/2017.
+//  Copyright © 2017 zhangyun. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
